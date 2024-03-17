74f9f294e22c2c372fa031cbfbc732d0d2d78c42
tuananh <nguyentuananhfedev@gmail.com>
2024-03-17 22:15:38 +0700

    router

--- UI/src/main.jsx
+++ UI/src/main.jsx
@@ -2,24 +2,28 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import 'primeicons/primeicons.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ListRouter from './components/Router/index.jsx';
-
+import { PrimeReactProvider } from 'primereact/api';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} >
-          {ListRouter.map(Rout => (
-            <Route key={Rout.id} path={Rout.path} element={Rout.component}></Route>
-          ))}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <PrimeReactProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} >
+            {ListRouter.map(Rout => (
+              <Route key={Rout.id} path={Rout.path} element={Rout.component}></Route>
+            ))}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </PrimeReactProvider>
   </React.StrictMode >,
 )
